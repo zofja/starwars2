bf34293c11474b8e59a7c0a2ea8347151cb01fb0
Zofia Partyka <zofiapartyka1@gmail.com>
2019-01-13 22:02:34 +0100

    important touch ups

--- src/battle.cc
+++ src/battle.cc
@@ -9,6 +9,7 @@ has been destroyed, ImperialStarships
 have driven the Rebel forces
 from their hidden base and pursued
 them across the galaxy.
+
 Evading the dreaded Imperial
 SoloStarships and Squadrons,
 a group of freedom fighters
@@ -16,6 +17,7 @@ led by Luke Skywalker has
 established a new secret
 base on the remote ice world
 of Hoth.
+
 The evil lord Darth Vader,
 obsessed with finding young
 Skywalker, has dispatched
